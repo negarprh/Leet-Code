08162bee1cb2f28a2e1a04793ecb959a7ee63147
Negar Pirasteh <negarpr@hotmail.com>
2024-10-25 22:35:35 -0400

    first commit

---
+++ Array/35. Search Insert Position/35_search_insert_position.py.py
@@ -0,0 +1,17 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        begin_index = 0 # first index in the list
+        end_index = (len(nums) - 1) # Last index in the list which made from then length
+        
+        while begin_index <= end_index :
+            mid_index = begin_index + (end_index - begin_index) // 2
+            mid_index_value = nums[mid_index]
+            if target == mid_index_value:
+                return mid_index
+            elif target < mid_index_value:
+                end_index = mid_index - 1
+            else:
+                begin_index = mid_index + 1
+        return begin_index
+
+        
